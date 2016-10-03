0d5976c6b56c05a9bd4182d9862b2b2205871df4
Johann Wilfling <johann.wilfling@gmail.com>
2016-10-03 17:49:11 +0200

    Added pod for example application.

---
+++ Example/Pods/Target Support Files/Pods-VegurFontiOS_Example/Pods-VegurFontiOS_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VegurFontiOS_Example : NSObject
+@end
+@implementation PodsDummy_Pods_VegurFontiOS_Example
+@end
---
+++ Example/Pods/Target Support Files/VegurFontiOS/VegurFontiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VegurFontiOS : NSObject
+@end
+@implementation PodsDummy_VegurFontiOS
+@end
